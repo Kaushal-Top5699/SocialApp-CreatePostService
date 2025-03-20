e8833c348b54b5debfb52a9179fee28d38cbcd2c
Kaushal Topinkatti B <kaushaltopinkattib@mac.lan>
2025-03-19 19:38:33 -0500

    CRUD operations added.

--- src/main/java/com/kaushal/Create_Post_Service/controller/PostController.java
+++ src/main/java/com/kaushal/Create_Post_Service/controller/PostController.java
@@ -7,12 +7,47 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 @RequestMapping("/post")
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @GetMapping("/get-all-posts")
+    public ResponseEntity<List<Post>> findAllPosts(@RequestHeader ("Authorization") String authHeader,
+                                                   @RequestParam String email) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        String token = authHeader.replace("Bearer ", "").trim();
+        System.out.println("Critical checks begin...");
+        List<Post> listOfPosts = postService.getAllPosts(token, email);
+        if (!listOfPosts.isEmpty()) {
+            return new ResponseEntity<>(listOfPosts, HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/find-post-by-id")
+    public ResponseEntity<HashMap<String, String>> findPostById(@RequestHeader ("Authorization") String authHeader,
+                                                                @RequestParam String email,
+                                                                @RequestHeader("postID") String postID) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        String token = authHeader.replace("Bearer ", "").trim();
+        System.out.println("Critical checks begin...");
+
+        HashMap<String, String> foundPost = postService.getPostById(token, email, postID);
+        if (foundPost == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(foundPost, HttpStatus.FOUND);
+    }
 
     @PostMapping("/create-post")
     public ResponseEntity<String> createPost(@RequestHeader("Authorization") String authHeader,
@@ -27,5 +62,37 @@ public class PostController {
             return new ResponseEntity<>("Post Created!", HttpStatus.CREATED);
         }
         return new ResponseEntity<>("Failed to create post.", HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/update-post")
+    public ResponseEntity<String> updatePost(@RequestHeader("Authorization") String authHeader,
+                                             @RequestHeader("postID") String postID,
+                                             @RequestParam String email, @RequestBody Post updatedPost) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        String token = authHeader.replace("Bearer ", "").trim();
+        System.out.println("Critical check for post update begin...");
+        boolean response = postService.updatePost(updatedPost, email, token, postID);
+        if (response) {
+            return new ResponseEntity<>("Post Updated!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Failed to update post.", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/delete-post")
+    public ResponseEntity<String> deletePost(@RequestHeader("Authorization") String authHeader,
+                                              @RequestParam String email,
+                                              @RequestHeader("postID") String postID) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        String token = authHeader.replace("Bearer ", "").trim();
+        System.out.println("Critical check for post update begin...");
+        boolean response = postService.deletePost(token, email, postID);
+        if (response) {
+            return new ResponseEntity<>("Post Deleted!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Failed to delete post.", HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/kaushal/Create_Post_Service/service/PostService.java
+++ src/main/java/com/kaushal/Create_Post_Service/service/PostService.java
@@ -2,6 +2,7 @@ package com.kaushal.Create_Post_Service.service;
 
 import com.kaushal.Create_Post_Service.entity.Post;
 import com.kaushal.Create_Post_Service.repository.PostRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 
 import java.sql.SQLOutput;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -23,15 +26,8 @@ public class PostService {
         this.webClient = webClientBuilder.baseUrl("http://localhost:8081/").build();
     }
 
-    public Boolean createPost(Post newPost, String email, String token) {
-        System.out.println("createPost() method called in PostService...");
-
-        if (email.isEmpty()) {
-            System.out.println("Email is empty, returning false.");
-            return false;
-        }
-
-        boolean isValid = Boolean.TRUE.equals(webClient.get()
+    private boolean isTokenValid(String token, String email) {
+        return Boolean.TRUE.equals(webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/gateway/check-token-validity")
                         .queryParam("email", email)
@@ -42,6 +38,34 @@ public class PostService {
                 .doOnNext(valid -> System.out.println("WebClient Response: " + valid)) // ✅ Print inside reactive pipeline
                 .doOnError(error -> System.out.println("WebClient Error: " + error.getMessage())) // ✅ Print errors
                 .block());
+    }
+
+    public List<Post> getAllPosts(String token, String email) {
+        System.out.println("createPost() method called in PostService...");
+        if (email.isEmpty()) {
+            System.out.println("Email is empty, returning false.");
+            return null;
+        }
+
+        boolean isValid = this.isTokenValid(token, email);
+        System.out.println("Token validity check result: " + isValid);
+        if (!isValid) {
+            System.out.println("Token is invalid, returning false.");
+            return null;
+        }
+        System.out.println("All Checks Done!");
+        return postRepository.findAll();
+    }
+
+    public Boolean createPost(Post newPost, String email, String token) {
+        System.out.println("createPost() method called in PostService...");
+
+        if (email.isEmpty()) {
+            System.out.println("Email is empty, returning false.");
+            return false;
+        }
+
+        boolean isValid = this.isTokenValid(token, email);
 
         System.out.println("Token validity check result: " + isValid);
 
@@ -62,5 +86,104 @@ public class PostService {
         return true;
     }
 
+    public boolean updatePost(Post updatedPost, String email, String token, String postID) {
+        System.out.println("updatePost() method called in PostService...");
 
+        if (email.isEmpty()) {
+            System.out.println("Email is empty, returning false.");
+            return false;
+        }
+
+        boolean isValid = this.isTokenValid(token, email);
+        System.out.println("Token validity check result: " + isValid);
+        if (!isValid) {
+            System.out.println("Token is invalid, returning false.");
+            return false;
+        }
+        System.out.println("All Checks Done!");
+
+
+        if (postID == null) {
+            return false;
+        }
+        ObjectId id = new ObjectId(postID);
+        Optional<Post> foundPost = postRepository.findById(id);
+        if (foundPost.isPresent()) {
+            if (updatedPost.getCaption() == null) {
+                System.out.println("Updated Post is null, returning false.");
+                return false;
+            }
+            Post currentPost = foundPost.get();
+            currentPost.setCaption(updatedPost.getCaption());
+            postRepository.save(currentPost);
+            System.out.println("Post updated successfully!");
+            return true;
+        }
+        return false;
+    }
+
+    public HashMap<String, String> getPostById(String token, String email, String postID) {
+        System.out.println("getPostById() method called in PostService...");
+        if (email.isEmpty()) {
+            System.out.println("Email is empty, returning false.");
+            return null;
+        }
+
+        boolean isValid = this.isTokenValid(token, email);
+        System.out.println("Token validity check result: " + isValid);
+        if (!isValid) {
+            System.out.println("Token is invalid, returning false.");
+            return null;
+        }
+        System.out.println("All Checks Done!");
+
+        if (postID == null) {
+            return null;
+        }
+
+        ObjectId id = new ObjectId(postID);
+        Optional<Post> foundPost = postRepository.findById(id);
+        if (foundPost.isPresent()) {
+            Post post = foundPost.get();
+            HashMap<String, String> postInfo = new HashMap<>();
+            postInfo.put("ObjectID", post.getId().toString());
+            postInfo.put("imageURL", post.getImageURL());
+            postInfo.put("userImageURL", post.getUserImageURL());
+            postInfo.put("userEmail", post.getUserEmail());
+            postInfo.put("username", post.getUsername());
+            postInfo.put("totalLikes", post.getTotalLikes());
+            postInfo.put("totalComments", post.getTotalComments());
+            postInfo.put("caption", post.getCaption());
+            return postInfo;
+        }
+        return null;
+    }
+
+    public boolean deletePost(String token, String email, String postID) {
+        System.out.println("deletePost() method called in PostService...");
+        if (email.isEmpty()) {
+            System.out.println("Email is empty, returning false.");
+            return false;
+        }
+
+        boolean isValid = this.isTokenValid(token, email);
+        System.out.println("Token validity check result: " + isValid);
+        if (!isValid) {
+            System.out.println("Token is invalid, returning false.");
+            return false;
+        }
+        System.out.println("All Checks Done!");
+
+        if (postID == null) {
+            return false;
+        }
+
+        ObjectId id = new ObjectId(postID);
+        Optional<Post> foundPost = postRepository.findById(id);
+        if (foundPost.isPresent()) {
+            postRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
